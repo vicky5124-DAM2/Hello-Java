afd6352563bd16bcfb0241bbb6af14149185871c
vickyf5124 <vickyf5124@gmail.com>
2022-09-08 20:08:12 +0200

    Initial commit

---
+++ src/com/vicky/Main.java
@@ -0,0 +1,12 @@
+package com.vicky;
+
+public class Main {
+    /**
+     * The main func
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        var txt = String.format("test %d", 123);
+        System.out.println(txt);
+    }
+}
